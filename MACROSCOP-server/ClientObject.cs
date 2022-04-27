a75336f2c1a6edb99eddebe2815b57156d00d970
Michael <68405115+Mishania12let@users.noreply.github.com>
2022-04-27 17:12:27 +0500

    Файлы приложения сервера
    сервер, как и клиент, по умолчанию крутятся локально на одном компьютере!

---
+++ MACROSCOP-server/ClientObject.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MACROSCOP_backend
+{
+    public class ClientObject
+    {
+        private TcpClient client;
+        private NetworkStream ns;
+        public ClientObject(TcpClient tcpClient)
+        {
+            client = tcpClient;
+            ns = client.GetStream();
+        }
+        public void Process()
+        {
+            try
+            {
+                // Buff for receiving data
+                byte[] data = new byte[64];
+
+                StringBuilder builder = new();
+                int bytes = 0;
+
+                // Receiving the message
+                do
+                {
+                    bytes = ns.Read(data, 0, data.Length);
+                    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                }
+                while (ns.DataAvailable && ns.CanRead);
+
+                string message = builder.ToString();
+                Console.WriteLine($"Client: {client.Client.RemoteEndPoint}: {message}");
+
+                bool isPalindromResult = IsPalindrom(message.Trim().ToCharArray());
+                string stringToClient = message.Trim() + " : " + isPalindromResult.ToString().Trim().ToUpper();
+
+                // Imitate long work
+                Thread.Sleep(2000);
+                // Send result
+                SendMessage(ns, stringToClient);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine($"Завершил работу с клиентом {client.Client.RemoteEndPoint}\n");
+                if (client.Connected)
+                    Close(client.GetStream());
+            }
+        }
+        private void Close(NetworkStream ns)
+        {
+            if (ns != null)
+                ns.Close();
+            if (client != null)
+                client.Close();
+        }
+        // Sends text in current NetworkStream
+        public static void SendMessage(NetworkStream ns, string text)
+        {
+            byte[] data = Encoding.Unicode.GetBytes(text);
+            ns.WriteAsync(data, 0, data.Length);
+        }
+        private static bool IsPalindrom(char[] text)
+        {
+            int lenText = text.Length;
+            for (int i = 0; i < text.Length / 2; i++)
+            {
+                if (text[i] != text[lenText - i - 1]) 
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ MACROSCOP-server/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MACROSCOP_backend
+{
+    public class Program
+    {
+        // Count of requests that server can handle in one time
+        public static int maxRequestCount { get; private set; }
+        // Current requests count. Increments or decrements in each client with mutexObject
+        public static int currentRequestCount { get; set; } = 0;
+        public static Mutex mutexObject = new();
+        const int port = 8888;
+
+        static TcpListener listener;
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Введите максимальное кол-во одновременно обрабатываемых запросов сервером: ");
+            maxRequestCount = InputDigit();
+            // Runs the server
+            await ServerRun();
+        }
+        private static async Task ServerRun()
+        {
+            try
+            {
+                listener = new TcpListener(IPAddress.Any, port);
+                listener.Start();
+
+                // For a future use
+                CancellationToken cancellationTokenSource = new();
+
+                while (!cancellationTokenSource.IsCancellationRequested)
+                {
+                    Console.WriteLine("Ожидание подключений...");
+                    // Accepting new client
+                    var client = await listener.AcceptTcpClientAsync();
+                    _ = Task.Run(async () =>
+                    {
+                        await HandleTcpClientAsync(client);
+                    });
+                    // Run new thread to work with client
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (listener != null)
+                    listener.Stop();
+            }
+        }
+        // Client logic starts here
+        private static async Task HandleTcpClientAsync(TcpClient client)
+        {
+            using NetworkStream ns = client.GetStream();
+            Console.WriteLine($"Подключил клиента: {client.Client.RemoteEndPoint}");
+
+            // Get current requests count
+            mutexObject.WaitOne();
+            int current = currentRequestCount++;
+            mutexObject.ReleaseMutex();
+
+            // Checking if server can work with one more client
+            // If is true disconnects the client
+            if (current >= maxRequestCount)
+            {
+                string stringToClient = "Server is busy!";
+                byte[] data = Encoding.Unicode.GetBytes(stringToClient);
+                
+                // Write the data so the client gets the message
+                ns.WriteAsync(data, 0, data.Length);
+
+                mutexObject.WaitOne();
+                currentRequestCount--;
+                mutexObject.ReleaseMutex();
+
+                Console.WriteLine($"Disconnected the {client.Client.RemoteEndPoint} - server busy.");
+                client.GetStream().Close();
+                client.Close();
+            }
+            else
+            {
+                ClientObject obj = new(client);
+                // Send that server is not busy now
+                ClientObject.SendMessage(ns, "Connected to the server");
+                
+                // Start synchronous client Process
+                obj.Process();
+
+                mutexObject.WaitOne();
+                currentRequestCount--;
+                mutexObject.ReleaseMutex();
+            }
+        }
+        private static int InputDigit()
+        {
+            bool success = false;
+            int number;
+            do
+            {
+                string text = Console.ReadLine();
+                success = int.TryParse(text, out number);
+                if (!success) Console.WriteLine($"Can't parse '{text}' into the 'int' format. Repeat the input: ");
+            } while (!success);
+            return number;
+        }
+    }
+}
